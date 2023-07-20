d0af0d0a59a3d844d6858cd96e6b2820b7807bf2
Shakir <shakirali.sacube@gmail.com>
2023-07-20 13:25:29 +0530

    Improved state management and data handling with Jotai and Suspense

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { Navigate } from "react-router-dom"
-import { useAtom, userInfoAtom, accessTokenAtom } from "./auth.js"
+import { useAtom, accessTokenAtom, isUserValidAtom} from "./auth.js"
 import AppRoutes from "./routes.js"
 
 import "./App.css"
 
 function App() {
   const [accessToken, setAccessToken] = useAtom(accessTokenAtom)
-  const [userInfo, setUserInfo] = useAtom(userInfoAtom)
+  const [isUserValid] = useAtom(isUserValidAtom)
 
-  if (accessToken === "" || !userInfo) {
+  if (accessToken === "" || !isUserValid) {
     console.log("Home: Navigating to -> /login")
     return <Navigate to="/login" />
   } else {
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react"
 import { useNavigate } from "react-router-dom"
 import { authenticate } from "../auth.js"
 import AppRoutes from "../routes.js"
-import { useAtom, accessTokenAtom, userInfoAtom } from "../auth.js"
+import { useAtom, accessTokenAtom, userInfoAtom, isUserValidAtom } from "../auth.js"
 
 export default function Login() {
   const navigator = useNavigate()
@@ -10,35 +10,31 @@ export default function Login() {
   const [password, setPassword] = useState("")
 
   const [accessToken, setAccessToken] = useAtom(accessTokenAtom)
-  const [userInfo, setUserInfo] = useAtom(userInfoAtom)
-
-  const [isAuthenticated, setIsAuthenticated] = useState(userInfo.email ? true : false)
+  const [userInfo] = useAtom(userInfoAtom)
+  const [isUserValid] = useAtom(isUserValidAtom)
 
   async function handleLogin(e, username, password) {
     // Get new token by supplying username and password
     e.preventDefault()
-    const tokenInfo = await authenticate(username, password)
+    const newToken = await authenticate(username, password)
 
-    // If tokenInfo.access_token exists, the authentication was successful
-    if (tokenInfo.access_token) {
-      setAccessToken(tokenInfo.access_token)
-      setIsAuthenticated(true)
-    } else {
-      console.log("Authentication Failed")
-    }
+    // If newToken.access_token exists, the authentication was successful
+    newToken.access_token
+      ? setAccessToken(newToken.access_token)
+      : console.log("Authentication Failed")
   }
 
   // Run when authentication status changes
   useEffect(() => {
-    if (isAuthenticated) {
+    if (isUserValid) {
       console.log(
         "Authentication Status: Successful Redirecting to " + AppRoutes[userInfo.role]
       )
       navigator(AppRoutes[userInfo.role]) // Navigate to user's dashboard
     } else console.log("Authentication Status: Failed")
-  }, [isAuthenticated])
+  }, [isUserValid])
 
-  if (!isAuthenticated)
+  if (!isUserValid)
     return (
       <form style={{ display: "flex", flexDirection: "column", gap: "2rem" }}>
         <label htmlFor="username">
